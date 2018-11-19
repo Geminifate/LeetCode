8685b8303e8091f63867ceb4da98d375ba8139f1
Mengfei.Wu <15295501589@163.com>
2018-11-19 12:24:05 +0800

    Signed-off-by: Mengfei.Wu <15295501589@163.com>

--- No521_Longest Uncommon Subsequence I/src/Solution.java
+++
@@ -1,9 +0,0 @@
-
-public class Solution {
-	public int findLUSlength(String a, String b) {
-        if (a.equals(b)) {
-			return -1;
-		}
-        return a.length()>=b.length()? a.length():b.length();
-    }
-}
