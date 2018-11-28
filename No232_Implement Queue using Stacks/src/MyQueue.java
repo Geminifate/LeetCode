a76c49f5436c22c2643c14b0459184fbc36d2ca6
Mengfei.Wu <15295501589@163.com>
2018-11-28 17:34:54 +0800

    update on 11/28

---
+++ No232_Implement Queue using Stacks/src/MyQueue.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+
+public class MyQueue {
+	private ArrayList<Integer> list;
+    /** Initialize your data structure here. */
+    public MyQueue() {
+        list = new ArrayList();
+    }
+    
+    /** Push element x to the back of queue. */
+    public void push(int x) {
+        list.add(x);
+    }
+    
+    /** Removes the element from in front of queue and returns that element. */
+    public int pop() {
+        int num=list.get(0);
+        list.remove(0);
+        return num;
+    }
+    
+    /** Get the front element. */
+    public int peek() {
+        return list.get(0);
+    }
+    
+    /** Returns whether the queue is empty. */
+    public boolean empty() {
+        return list.isEmpty();
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
